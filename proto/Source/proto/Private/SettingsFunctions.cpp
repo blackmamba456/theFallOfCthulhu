19caf5827f1c2ca56fdb804296cc290bb00edd7a
Jan Oldsen <jan@oldsen.de>
2015-02-09 15:24:56 +0100

    - saving and loading configs - options in pause menu

--- proto/Source/proto/Private/SettingsFunctions.cpp
+++ proto/Source/proto/Private/SettingsFunctions.cpp
@@ -2,6 +2,7 @@
 
 #include "proto.h"
 #include "SettingsFunctions.h"
+#include "Engine.h"
 
 USettingsFunctions::USettingsFunctions(const class FPostConstructInitializeProperties& PCIP)
 	: Super(PCIP)
@@ -15,4 +16,43 @@ USettingsFunctions::USettingsFunctions(const class FPostConstructInitializePrope
 void USettingsFunctions::changeVolume(USoundClass* soundClass, float volume)
 {
 	soundClass->Properties.Volume = volume;
+}
+
+
+void USettingsFunctions::saveSoundSettings(ASoundConfigs* settings)
+{
+	settings->SaveConfig();
+}
+
+void USettingsFunctions::loadSoundSettings(ASoundConfigs* settings)
+{
+	settings->LoadConfig();
+}
+
+
+void USettingsFunctions::setResolution(int32 x, int32 y, int fullscreen)
+{
+	GEngine->GetGameUserSettings()->SetScreenResolution(FIntPoint(x, y));
+	GEngine->GetGameUserSettings()->SetFullscreenMode((EWindowMode::Type)fullscreen);
+}
+
+void USettingsFunctions::applyGraphics()
+{
+	GEngine->GetGameUserSettings()->ApplySettings(true);
+}
+
+
+FResolution USettingsFunctions::getResolution()
+{
+	FResolution resolution;
+	FIntPoint res = GEngine->GetGameUserSettings()->GetScreenResolution();
+	if (res == FIntPoint(0, 0))
+		res = GEngine->GetGameUserSettings()->GetDefaultResolution();
+
+
+	resolution.resolutionX = res.X;
+	resolution.resolutionY = res.Y;
+	resolution.fullscreen = (int32)GEngine->GetGameUserSettings()->GetFullscreenMode();
+
+	return resolution;
 }
--- proto/Source/proto/Public/SettingsFunctions.h
+++ proto/Source/proto/Public/SettingsFunctions.h
@@ -2,8 +2,26 @@
 
 #pragma once
 
+#include "SoundConfigs.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "SettingsFunctions.generated.h"
+
+USTRUCT()
+struct FResolution
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = Default)
+	int32 resolutionX;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = Default)
+	int32 resolutionY;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = Default)
+	int32 fullscreen;
+};
+
+
+
+
 
 
 /**
@@ -17,6 +35,23 @@ class PROTO_API USettingsFunctions : public UBlueprintFunctionLibrary
 	UFUNCTION(BlueprintCallable, Category = "SoundSettings")
 	static void changeVolume(USoundClass* soundClass, float volume);
 
+	UFUNCTION(BlueprintCallable, Category = "SoundSettings")
+	static void saveSoundSettings(ASoundConfigs* settings);
+
+	UFUNCTION(BlueprintCallable, Category = "SoundSettings")
+	static void loadSoundSettings(ASoundConfigs* settings);
+
+
+	UFUNCTION(BlueprintCallable, Category = "GraphicsSettings")
+	static void setResolution(int32 x, int32 y, int32 fullscreen);
+
+	UFUNCTION(BlueprintCallable, Category = "GraphicsSettings")
+	static FResolution getResolution();
+
+
+	UFUNCTION(BlueprintCallable, Category = "GraphicsSettings")
+	static void applyGraphics();
 
 };
+
 
